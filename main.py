21e3d3b8dbe0f147c5b472c2d4c8b991dedc7b48
Joo Hyoung Cha <aoikazto@naver.com>
2024-09-22 17:32:51 +0900

    update code and ppt

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__) 
 @app.route('/') 
 def hello_world(): 
-    return "<p>Hello, GIST Developers's Night</p>"
+    return "<p>GIST Developers's Night</p>"
 
 app.run(host='0.0.0.0')
